8bec7c97d952f6a6a0be79569a6701bc83e3a956
mcooper <mw.coop.r@gmail.com>
2020-12-05 00:21:17 +0000

    get predictions from fixest

--- model/fixest.R
+++ model/fixest.R
@@ -1,17 +1,18 @@
-## This looks like it will work, but is too large for an E16
+##E32
 
 library(data.table)
 library(fixest)
+library(dplyr)
+library(ggplot2)
 
 setwd('~/tweets/')
 
 data <- fread('all.csv')
 
 #Tempknots
-knots = list("temp.hi"= c(min(data$temp.hi), -10, 0, 5, 10, 15, 20, 25, 30, 35, 
-                          max(data$temp.hi)),
-             "precip"= c(min(data$precip), 0.000001, 0.05, 0.5, max(data$precip)),
-             "srad"= c(min(data$srad), 0.000001, 250, 500, 1000, max(data$srad)))
+knots = list("temp.hi"= c(-10, 0, 5, 10, 15, 20, 25, 30, 35),
+             "precip"= c(0.000001, 0.05, 0.5),
+             "srad"= c(0.000001, 250, 500, 1000))
 
 #Define Segmenting Function
 piece.formula <- function(var.name, knots) {
@@ -22,13 +23,39 @@ piece.formula <- function(var.name, knots) {
               collapse = " + ", sep=""))
 }
 
-form <- paste0("vader ~ ", piece.formula("temp.hi", knots[['temp.hi']]),
-                           #piece.formula("precip", knots[['precip']]),
-                           #piece.formula("srad", knots[['srad']]),
-                           " | dow + doy + tod + fips + year + statemonth")
 
-start <- Sys.time()
-print(start)
+system('~/telegram.sh "starting"')
+form <- paste0("vader ~ ", piece.formula("temp.hi", knots[['temp.hi']]), ' + ',
+                           piece.formula("precip", knots[['precip']]), ' + ',
+                           piece.formula("srad", knots[['srad']]),
+                           " | dow + doy + tod + fips + year + statemonth")
 mod <- feols(as.formula(form), data)
-end <- Sys.time()
-end - start
+system('~/telegram.sh "donezo!"')
+
+pred <- bind_rows(data.frame(srad=0, precip=0, temp.hi=seq(-40, 50)),
+                  data.frame(temp.hi=0, precip=0, srad=c(seq(0, 0.05, length.out=10),
+                                                         seq(0.05, 0.5, length.out=10),
+                                                         seq(0.5, 50, length.out=10))),
+                  data.frame(temp.hi=0, srad=0, precip=c(seq(0, 250, length.out=10),
+                                                          seq(250, 500, length.out=10),
+                                                          seq(500, 1000, length.out=10),
+                                                          seq(1000, 2000, length.out=10))))
+
+pred$dow = data$dow[1]
+pred$doy = data$doy[1]
+pred$tod = data$tod[1]
+pred$fips = data$fips[1]
+pred$year = data$year[1]
+pred$statemonth = data$statemonth[1]
+
+res <- predict(mod, pred)
+pred$prediction <- res
+
+ggplot(pred %>% filter(srad==0, precip==0)) + 
+  geom_line(aes(x=temp.hi, y=prediction))
+
+ggplot(pred %>% filter(temp.hi==0, srad==0)) + 
+  geom_line(aes(x=log(precip), y=prediction))
+
+ggplot(pred %>% filter(temp.hi==0, precip==0)) + 
+  geom_line(aes(x=srad, y=prediction))
