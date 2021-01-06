7325ad4d5b600be704633ac6156b04742dce6e10
mcooper <mw.coop.r@gmail.com>
2021-01-06 01:05:52 +0000

    new figure for manuscript

--- viz/viz_bootstrap.R
+++ viz/viz_bootstrap.R
@@ -20,16 +20,8 @@ knots = list("wbgt"= c(min(data$wbgt), -10, 0, 5, 10, 15, 20, 25,
              "prcp"= c(min(data$prcp), 0.000001, 0.05, 0.5, max(data$prcp)),
              "srad"= c(min(data$srad), 0.000001, 250, 500, 1000, max(data$srad)))
 
-#Tempknots
-knots = list("wbgt"= c(-30, -10, 0, 5, 10, 15, 20, 25, 
-                          30),
-             "prcp"= c(0, 0.000001, 0.05, 0.5, 10),
-             "srad"= c(0, 0.000001, 250, 500, 1000, 1800))
-
-
 ######################################
 #wbgt
-
 piece.formula <- function(var.name, knots, interact_var='') {
   knots <- knots[c(-1, -length(knots))]
   formula.sign <- rep(" - ", length(knots))
@@ -72,7 +64,21 @@ make_groups <- function(df, label, values){
   final
 }
 
-preddf <- data.frame(wbgt=fill_knots(knots$wbgt))
+#######################################
+# Get plot accessories
+#######################################
+
+ref_temps <- NULL
+for (t in seq(-20, 30, by=10)){
+  ref_temps <- c(ref_temps, median(data$temp[data$wbgt > (t - 0.25) & data$wbgt < (t + 0.25)]))
+}
+ref_temps <- round(ref_temps)
+
+
+########################################
+# WBGT
+#########################################
+preddf <- data.frame(wbgt=seq(-22, 33))
 preddf <- make_groups(preddf, 'income_percap', qs[c(2, 11, 20)])
 
 preddf$vader <- 1
@@ -84,14 +90,13 @@ mm <- model.matrix(as.formula(paste0("vader ~ ",
 
 colnames(mm)[colnames(mm) == 'wbgt:income_percap'] <- 'income_percap:wbgt'
 mm <- mm[ , colnames(mm) != '(Intercept)']
-mm <- mm[ , colnames(mm) != 'income_percap']
+#mm <- mm[ , colnames(mm) != 'income_percap']
 
 mmp <- lapply(seq_len(nrow(mm)), function(i) mm[i,])
 
 
 #Iterate over models
 mods <- list.files()
-
 for (modf in mods){
 
   mod <- readRDS(modf)
@@ -116,21 +121,52 @@ levels(preddf$income_percap) <- paste0(levels(preddf$income_percap), ' (',
 
 preddf <- preddf %>%
   gather(key, value, -wbgt, -income_percap, -vader) %>%
-  mutate(group = paste0(income_percap, key))
+  #Normalize so that graph shows difference from wbgt = X
+  group_by(income_percap, key) %>%
+  mutate(value = value - value[wbgt == 0]) %>%
+  #Get ymin, ymax, and mean
+  group_by(wbgt, income_percap) %>%
+  summarize(ymin = quantile(value, probs=0.025),
+            ymax = quantile(value, probs=0.975),
+            pred = quantile(value, probs=0.5))
 
 curve <- ggplot(preddf) + 
-  geom_line(aes(x=wbgt, y=value, color=income_percap, group=group)) + 
-  scale_x_continuous(expand=c(0, 0), limits=c(min(knots$wbgt), max(knots$wbgt))) + 
+  geom_line(aes(x=wbgt, y=pred, color=income_percap)) + 
+  geom_ribbon(aes(x=wbgt, ymin=ymin, ymax=ymax, fill=income_percap), alpha=0.5) + 
+  scale_x_continuous(expand=c(0, 0), limits=c(-22, 33)) + 
+  #scale_y_continuous(breaks=seq(0, -0.02, -0.005)) + 
   labs(x='', y='Change in Mood of Tweet',
        fill = 'Census Block\nIncome Per-Capita\n(Percentile)',
        color = 'Census Block\nIncome Per-Capita\n(Percentile)') +
-  theme(legend.position=c(0.2, 0.2))
+  theme_classic() + 
+  theme(legend.position=c(0.3, 0.4),
+        axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.margin = unit(c(0.025, 0.025, -1, 0.025), "cm"),
+        panel.grid.major = element_line(color = "lightgrey", size=0.5),
+        panel.grid.minor = element_line(color = "lightgrey", size=0.25))
 
 hist <- ggplot(data[sample(1:nrow(data), nrow(data)*0.01), 'wbgt']) +
   geom_histogram(aes(x=wbgt), binwidth=1) + 
-  scale_x_continuous(expand=c(0, 0), limits=c(min(knots$wbgt), max(knots$wbgt))) + 
-  scale_y_continuous(labels=function(x){format(x*100, big.mark=',')}) + 
-  labs(y="Tweet Count", x="Wet Bulb Global Temperature")
+  scale_x_continuous(expand=c(0, 0), limits=c(-22, 33)) + 
+  scale_y_continuous(labels=function(x){format(x*100, big.mark=',')}, expand=c(0,0),
+                     breaks=c(75000)) + 
+  labs(y="Tweet\nCount", x="Wet Bulb Globe Temperature (C)") + 
+  theme_classic() + 
+  theme(panel.grid.major = element_line(color = "lightgrey", size=0.5),
+        axis.text.y = element_blank(),
+        axis.ticks.y = element_blank(),
+        panel.grid.minor = element_line(color = "lightgrey", size=0.25))
 
-plot_grid(curve, hist, align='v', axis='rl', ncol=1, rel_heights=c(0.8, 0.2))
-ggsave('~/temp-sentiment/res/wbgt-income-ref-segments-bootstrap.png', width=6, height=7)
+
+x2 <- ggplot() +
+  scale_x_continuous(expand=c(0, 0), limits=c(-22, 33), labels=ref_temps) + 
+  labs(x="Median Dry Bulb (Normal) Temperature (C)") + 
+  theme_classic()
+
+x <- get_x_axis(x2)
+xl <- get_plot_component(x2, "xlab-b")
+
+plot_grid(curve, hist, ggdraw(x), ggdraw(xl), align='v', axis='rl', ncol=1, 
+          rel_heights=c(0.8, 0.2, 0.04, 0.04))
+ggsave('~/temp-sentiment/res/wbgt-income.png', width=6, height=5)
