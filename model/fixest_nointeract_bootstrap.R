437d66e2263669e159b58b3c54c30213627a927f
mcooper <mw.coop.r@gmail.com>
2021-01-23 20:11:01 +0000

    clean up models; run no interaction bootstrap

---
+++ model/fixest_nointeract_bootstrap.R
@@ -0,0 +1,59 @@
+library(data.table)
+library(fixest)
+library(tidyverse)
+
+RUN <- 'wbgt_nointeract'
+
+setwd('~/tweets/')
+
+data <- fread('all.csv')
+
+data <- data[weather_term == 0, ]
+
+#Tempknots
+knots = list("wbgt"= c(min(data$wbgt), -10, 0, 5, 10, 15, 20, 25, 
+                          max(data$wbgt)))
+
+data$raining <- data$prcp > 0
+
+#Save memory by deleting useless columns!
+data <- data[ , c('wbgt', 'raining', 'srad', 'vader', 'fips', 'year', 
+                  'statemonth', 'tod', 'doy', 'dow')]
+
+#Define Segmenting Function
+piece.formula <- function(var.name, knots, interact_var='') {
+  knots <- knots[c(-1, -length(knots))]
+  formula.sign <- rep(" - ", length(knots))
+  formula.sign[knots < 0] <- " + "
+  if (interact_var == ''){
+    paste(var.name, "+",
+        paste("I(pmax(", var.name, formula.sign, abs(knots), ", 0))",
+              collapse = " + ", sep=""))
+  } else{
+    paste(interact_var, '*', var.name, "+",
+        paste("I(", interact_var, "*pmax(", var.name, formula.sign, abs(knots), ", 0))",
+              collapse = " + ", sep=""))
+  }
+}
+
+formula <- paste0("vader ~ ", 
+                     piece.formula("wbgt", knots[['wbgt']], ""), ' + ',
+                     'raining + srad',
+                     " | dow + doy + tod + fips + year + statemonth")
+
+for (i in 1:80){
+  mod <- feols(as.formula(formula), data[sample(1:nrow(data), nrow(data), replace=T), ])
+  cf <- coef(mod)
+  vc <- vcov(mod)
+  myobj <- list(vcov=vc, coef=cf)
+  class(myobj) <- 'bootmod'
+  saveRDS(myobj, file=paste0('bootstrap/', RUN, '/', Sys.time()))
+  system(paste0('~/telegram.sh "Did a business ', i, '"'))
+  rm(list=c('mod', 'cf', 'vc', 'myobj'))
+  gc()
+}
+
+
+system('sudo poweroff')
+
+
