a21758896b7b1e33b89dbc7d10af2c88216a80f6
mcooper <mw.coop.r@gmail.com>
2021-04-15 02:03:05 +0000

    CSA and hourly analyses

---
+++ csa/model_space.R
@@ -0,0 +1,142 @@
+library(data.table)
+library(tidyverse)
+library(fixest)
+library(broom)
+library(sf)
+library(USAboundaries)
+
+data <- fread('~/tweets/all.csv')
+csadat <- fread('~/tweets/csa/fips-csa.csv')
+
+data <- merge(data, csadat, on='fips', all=F)
+
+data$month <- substr(data$statemonth, 4, 5)
+data$income_percap <- log(data$income_percap)
+data <- data[weather_term == 0, ]
+data$raining <- data$prcp > 0
+
+qs <- quantile(data$income_percap, seq(0, 1, by=0.05))
+race_qs <- quantile(data$race_white, seq(0, 1, by=0.05))
+
+csas <- tab(data$id)
+csas <- as.numeric(names(csas))[order(csas, decreasing=T)]
+
+#############################################
+# Now effects of temp*income and temp*race
+#############################################
+
+allres <- data.frame()
+for (csasel in csas){
+  print(which(csas == csasel)/length(csas))
+
+  sel <- data[data$id == csasel & data$wbgt > 5, ]
+
+  mod <- feols(vader ~ wbgt + raining + srad | dow + doy + tod + fips + year + month, data=sel)
+  base_res <- tidy(mod) %>%
+    filter(term =='wbgt') %>%
+    mutate(id = csasel)
+
+  mod <- feols(vader ~ wbgt*income_percap + raining*income_percap + srad*income_percap | dow + doy + tod + fips + year + month, data=sel)
+  inc_res <- tidy(mod) %>%
+    filter(term =='wbgt:income_percap') %>%
+    mutate(id = csasel)
+
+  mod <- feols(vader ~ wbgt*race_white + raining*race_white + srad*race_white | dow + doy + tod + fips + year + month, data=sel)
+  race_res <- tidy(mod) %>%
+    filter(term == 'wbgt:race_white') %>%
+    mutate(id = csasel)
+
+  allres <- bind_rows(allres, base_res, inc_res, race_res)
+}
+
+#########################
+# Visualize
+############################
+csa_sf <- read_sf('~/tweets/csa', 'msa-csa')
+
+states <- us_states(resolution='low') %>%
+  filter(!state_name %in% c("Virgin Islands", "Alaska", "Hawaii", 
+                      "Guam", "Puerto Rico", "Northern Mariana Islands",
+                      "American Samoa"))
+
+#############################
+# Baseline results
+###########################
+
+m <- merge(csa_sf, allres %>% filter(term == 'wbgt'))
+m$estimate <- 25*(m$estimate)
+ggplot() + 
+  geom_sf(data = states) + 
+  geom_sf(data = m, aes(fill=estimate)) + 
+  geom_sf(data = states, fill=NA) + 
+  geom_sf(data = m, fill=NA, aes(color=p.value < 0.1, size=p.value < 0.1)) + 
+  scale_fill_gradient2(low='#ca0020', mid='#f7f7f7', high='#0571b0') + 
+  scale_color_manual(values=c('TRUE'='#000000', 'FALSE'=NA)) +
+  scale_size_manual(values=c(1, 1.1)) +
+  guides(color=FALSE, size=FALSE,
+         fill = guide_colourbar(title.position="top", title.hjust = 0.5)) + 
+  theme_void() + 
+  theme(legend.position=c(0.2, 0.1),
+        legend.direction='horizontal',
+        legend.key.width=unit(2, 'cm'),
+        legend.key.height=unit(1, 'cm'),
+        legend.text=element_text(size=rel(2)),
+        legend.title=element_text(size=rel(2))) + 
+  coord_sf(crs='epsg:2163') + 
+  labs(fill='Change in Sentiment')
+ggsave('~/temp-sentiment/res/map_wbgt.png', width=12, height=8)
+
+#############################
+# Income results
+###########################
+
+m <- merge(csa_sf, allres %>% filter(term == 'wbgt:income_percap'))
+m$estimate <- 25*(m$estimate/(qs[2] - qs[20]))
+ggplot() + 
+  geom_sf(data = states) + 
+  geom_sf(data = m, aes(fill=estimate)) + 
+  geom_sf(data = states, fill=NA) + 
+  geom_sf(data = m, fill=NA, aes(color=p.value < 0.1, size=p.value < 0.1)) + 
+  scale_fill_gradient2(low='#e66101', mid='#f7f7f7', high='#5e3c99', midpoint=0) + 
+  scale_color_manual(values=c('TRUE'='#000000', 'FALSE'=NA)) +
+  scale_size_manual(values=c(1, 1.1)) +
+  guides(color=FALSE, size=FALSE,
+         fill = guide_colourbar(title.position="top", title.hjust = 0.5)) + 
+  theme_void() + 
+  theme(legend.position=c(0.2, 0.1),
+        legend.direction='horizontal',
+        legend.key.width=unit(2, 'cm'),
+        legend.key.height=unit(1, 'cm'),
+        legend.text=element_text(size=rel(2)),
+        legend.title=element_text(size=rel(2))) + 
+  coord_sf(crs='epsg:2163') + 
+  labs(fill='Gap in Sentiment')
+ggsave('~/temp-sentiment/res/map_wbgt_income.png', width=12, height=8)
+
+#############################
+# Race results
+###########################
+
+m <- merge(csa_sf, allres %>% filter(term == 'wbgt:race_white'))
+m$estimate <- 25*(m$estimate/(race_qs[2] - race_qs[20]))
+ggplot() + 
+  geom_sf(data = states) + 
+  geom_sf(data = m, aes(fill=estimate)) + 
+  geom_sf(data = states, fill=NA) + 
+  geom_sf(data = m, fill=NA, aes(color=p.value < 0.1, size=p.value < 0.1)) + 
+  scale_fill_gradient2(low='#d01c8b', mid='#f7f7f7', high='#4dac26', midpoint=0) + 
+  scale_color_manual(values=c('TRUE'='#000000', 'FALSE'=NA)) +
+  scale_size_manual(values=c(1, 1.1)) +
+  guides(color=FALSE, size=FALSE,
+         fill = guide_colourbar(title.position="top", title.hjust = 0.5)) + 
+  theme_void() + 
+  theme(legend.position=c(0.2, 0.1),
+        legend.direction='horizontal',
+        legend.key.width=unit(2, 'cm'),
+        legend.key.height=unit(1, 'cm'),
+        legend.text=element_text(size=rel(2)),
+        legend.title=element_text(size=rel(2))) + 
+  coord_sf(crs='epsg:2163') + 
+  labs(fill='Gap in Sentiment')
+ggsave('~/temp-sentiment/res/map_wbgt_race.png', width=12, height=8)
+
---
+++ csa/model_ts.R
@@ -0,0 +1,171 @@
+library(data.table)
+library(tidyverse)
+library(fixest)
+library(broom)
+library(hms)
+library(cowplot)
+
+options(scipen=100)
+
+data <- fread('~/tweets/all.csv')
+
+data$month <- substr(data$statemonth, 4, 5)
+data$income_percap <- log(data$income_percap)
+data <- data[weather_term == 0, ]
+data$raining <- data$prcp > 0
+data$hour <- substr(data$tod, 1, 2)
+
+qs <- quantile(data$income_percap, seq(0, 1, by=0.05))
+race_qs <- quantile(data$race_white, seq(0, 1, by=0.05))
+
+tods <- tab(data$hour[data$wbgt > 5])
+tods <- data.frame(tod=names(tods), count=as.numeric(tods))
+
+#############################################
+# Now effects of temp*income and temp*race
+#############################################
+
+allres <- data.frame()
+for (todsel in tods$tod){
+  print(which(tods == todsel)/nrow(tods))
+
+  sel <- data[data$hour == todsel & data$wbgt > 5, ]
+
+  mod <- feols(vader ~ wbgt + raining + srad | dow + doy + tod + fips + year + month, data=sel)
+  base_res <- tidy(mod) %>%
+    filter(term =='wbgt') %>%
+    mutate(tod = todsel)
+
+   mod <- feols(vader ~ wbgt*income_percap + raining*income_percap + srad*income_percap | dow + doy + tod + fips + year + month, data=sel)
+   inc_res <- tidy(mod) %>%
+     filter(term =='wbgt:income_percap') %>%
+     mutate(tod = todsel)
+
+   mod <- feols(vader ~ wbgt*race_white + raining*race_white + srad*race_white | dow + doy + tod + fips + year + month, data=sel)
+   race_res <- tidy(mod) %>%
+     filter(term == 'wbgt:race_white') %>%
+     mutate(tod = todsel)
+
+  allres <- bind_rows(allres, base_res, inc_res, race_res)
+}
+
+#########################
+# Visualize
+############################
+
+allres$tod <- hms::as_hms(paste0(allres$tod, ':00:00'))
+tods$tods <-  hms::as_hms(paste0(tods$tod, ':00:00'))
+
+############################
+# Baseline effect
+############################
+
+m <- allres %>% filter(term == 'wbgt')
+m$estimate <- 25*(m$estimate)
+m$std.error <- 25*(m$std.error)
+
+m$max <- m$estimate + m$std.error*qnorm(0.975)
+m$min <- m$estimate + m$std.error*qnorm(0.025)
+
+gap <- ggplot(m) + 
+  geom_line(aes(x=tod, y=estimate), color='#FF0000') + 
+  geom_point(aes(x=tod, y=estimate), color='#FF0000') + 
+  geom_errorbar(aes(x=tod, ymin=min, ymax=max), color='#FF0000') + 
+  geom_hline(aes(yintercept=0), linetype=2) + 
+  scale_x_time(expand=c(0, 0), label=function(x){substr(as.character(x), 1, 5)},
+               breaks=hms::as_hms(paste0(seq(0, 23, 3), ':00:00'))) + 
+  theme_classic() + 
+  labs(y='Gap in Sentiment') + 
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.margin = unit(c(0.025, 0.025, -1, 0.025), "cm"))
+
+curve <- ggplot(tods) + 
+  geom_bar(aes(x=tods, y=count), stat='identity') + 
+  scale_x_time(expand=c(0, 0), label=function(x){substr(as.character(x), 1, 5)},
+               breaks=hms::as_hms(paste0(seq(0, 23, 3), ':00:00'))) + 
+  scale_y_continuous(labels=function(x){format(x, big.mark=',')}, expand=c(0,0),
+                     breaks=c(50000000)) + 
+  theme_classic() + 
+  labs(y="Tweets", x="Time of Day")
+
+plot_grid(gap, curve, align='v', axis='rl', ncol=1, 
+          rel_heights=c(0.8, 0.2))
+ggsave('~/temp-sentiment/res/ts.png', width=6, height=5)
+
+
+#############################
+# Income results
+###########################
+
+m <- allres %>% filter(term == 'wbgt:income_percap')
+m$estimate <- 25*(m$estimate/(qs[2] - qs[20]))
+m$std.error <- 25*(m$std.error/(qs[2] - qs[20]))
+
+m$max <- m$estimate + m$std.error*qnorm(0.975)
+m$min <- m$estimate + m$std.error*qnorm(0.025)
+
+gap <- ggplot(m) + 
+  geom_line(aes(x=tod, y=estimate), color='#006400') + 
+  geom_point(aes(x=tod, y=estimate), color='#006400') + 
+  geom_errorbar(aes(x=tod, ymin=min, ymax=max), color='#006400') + 
+  geom_hline(aes(yintercept=0), linetype=2) + 
+  scale_x_time(expand=c(0, 0), label=function(x){substr(as.character(x), 1, 5)},
+               breaks=hms::as_hms(paste0(seq(0, 23, 3), ':00:00'))) + 
+  theme_classic() + 
+  labs(y='Gap in Sentiment') + 
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.margin = unit(c(0.025, 0.025, -1, 0.025), "cm"))
+
+curve <- ggplot(tods) + 
+  geom_bar(aes(x=tods, y=count), stat='identity') + 
+  scale_x_time(expand=c(0, 0), label=function(x){substr(as.character(x), 1, 5)},
+               breaks=hms::as_hms(paste0(seq(0, 23, 3), ':00:00'))) + 
+  scale_y_continuous(labels=function(x){format(x, big.mark=',')}, expand=c(0,0),
+                     breaks=c(50000000)) + 
+  theme_classic() + 
+  labs(y="Tweets", x="Time of Day")
+
+plot_grid(gap, curve, align='v', axis='rl', ncol=1, 
+          rel_heights=c(0.8, 0.2))
+ggsave('~/temp-sentiment/res/ts_income.png', width=6, height=5)
+
+
+#############################
+# Race results
+###########################
+
+m <- allres %>% filter(term == 'wbgt:race_white')
+m$estimate <- 25*(m$estimate/(race_qs[2] - race_qs[20]))
+m$std.error <- 25*(m$std.error/(race_qs[2] - race_qs[20]))
+
+m$max <- m$estimate + m$std.error*qnorm(0.975)
+m$min <- m$estimate + m$std.error*qnorm(0.025)
+
+gap <- ggplot(m) + 
+  geom_line(aes(x=tod, y=estimate), color='#00008B') + 
+  geom_point(aes(x=tod, y=estimate), color='#00008B') + 
+  geom_errorbar(aes(x=tod, ymin=min, ymax=max), color='#00008B') + 
+  geom_hline(aes(yintercept=0), linetype=2) + 
+  scale_x_time(expand=c(0, 0), label=function(x){substr(as.character(x), 1, 5)},
+               breaks=hms::as_hms(paste0(seq(0, 23, 3), ':00:00'))) + 
+  theme_classic() + 
+  labs(y='Gap in Sentiment') + 
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank(),
+        plot.margin = unit(c(0.025, 0.025, -1, 0.025), "cm"))
+
+curve <- ggplot(tods) + 
+  geom_bar(aes(x=tods, y=count), stat='identity') + 
+  scale_x_time(expand=c(0, 0), label=function(x){substr(as.character(x), 1, 5)},
+               breaks=hms::as_hms(paste0(seq(0, 23, 3), ':00:00'))) + 
+  scale_y_continuous(labels=function(x){format(x, big.mark=',')}, expand=c(0,0),
+                     breaks=c(50000000)) + 
+  theme_classic() + 
+  labs(y="Tweets", x="Time of Day")
+
+plot_grid(gap, curve, align='v', axis='rl', ncol=1, 
+          rel_heights=c(0.8, 0.2))
+ggsave('~/temp-sentiment/res/ts_race.png', width=6, height=5)
+
