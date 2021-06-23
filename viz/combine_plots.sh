b38db942169fcc03420cc982eac985832731d32b
mcooper <mw.coop.r@gmail.com>
2021-06-23 17:33:52 -0400

    tweak figures

--- viz/combine_plots.sh
+++ viz/combine_plots.sh
@@ -4,8 +4,8 @@ mkdir conv
 cd conv
 
 #Add labels
-convert ../wbgt-income.png -gravity NorthWest -pointsize 125 -font "Arial-Bold" -annotate +25+25 'a' wbgt-income.png
-convert ../wbgt-race_q.png -gravity NorthWest -pointsize 125 -font "Arial-Bold" -annotate +25+25 'b' wbgt-race_q.png
+convert ../wbgt-income.png -gravity NorthWest -pointsize 125 -font "Arial-Bold" -annotate +10+10 'a' wbgt-income.png
+convert ../wbgt-race_q.png -gravity NorthWest -pointsize 125 -font "Arial-Bold" -annotate +10+10 'b' wbgt-race_q.png
 
 #Stack
 convert +append wbgt-income.png wbgt-race_q.png ../wbgt_combined.png
